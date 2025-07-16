d0989aad48dd6e9393e23f4436af95670c7ea697
Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
2025-07-16 11:41:05 -0700

    run prettier
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>

--- web/index.js
+++ web/index.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import fetch from 'node-fetch';
+import express from "express";
+import fetch from "node-fetch";
 
 const port = process.env.WEB_PORT || 8080;
 const ghostunnelOnePort = process.env.WEB_GHOSTUNNEL_ONE_PORT || 8081;
@@ -7,35 +7,35 @@ const ghostunnelTwoPort = process.env.WEB_GHOSTUNNEL_TWO_PORT || 8082;
 
 const app = express();
 
-app.get('/backend1', async (req, res) => {
+app.get("/backend1", async (req, res) => {
   let backendStatuses = {};
   try {
-    const responseOne = await fetch(`http://localhost:${ghostunnelOnePort}`)
+    const responseOne = await fetch(`http://localhost:${ghostunnelOnePort}`);
     const resJsonOne = await responseOne.json();
     backendStatuses.backend1 = resJsonOne;
 
     res.status(200).json(backendStatuses);
   } catch (err) {
     console.error(err);
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
 });
 
-app.get('/backend2', async (req, res) => {
+app.get("/backend2", async (req, res) => {
   let backendStatuses = {};
   try {
-    const responseTwo = await fetch(`http://localhost:${ghostunnelTwoPort}`)
+    const responseTwo = await fetch(`http://localhost:${ghostunnelTwoPort}`);
     const resJsonTwo = await responseTwo.json();
     backendStatuses.backend2 = resJsonTwo;
 
     res.status(200).json(backendStatuses);
   } catch (err) {
     console.error(err);
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
 });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
